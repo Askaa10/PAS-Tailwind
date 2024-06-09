4e40f88564d09e65d046b37b08ccd1ce98b332da
Askaa10 <149747430+Askaa10@users.noreply.github.com>
2024-06-09 10:38:58 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,40 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        f1:"#31A0FE",
+        TX1:"#F83535",
+        TX01:"#A4B5B8",
+        TX2:"#42424",
+        TX3:"#fdc62e",
+        TX4:"#F42C37",
+        TX5:"#D9D9D9",
+        TX6:"#3DDD6A",
+        TX7:"#1376f4",
+        TX8:"#D9D9D9",
+        TX9:"#1F2937",
+        TX10:"#273242",
+        TX11:"##ffffff0d",
+        b1:"#263B3E",
+        b2:"#273242",
+        b3:"#B22222",
+        b4:"#A4B5B8",
+        // warna buat yang di Account 
+        b5:"#F5F5F5",
+      },  
+      backgroundImage:{
+        bgl:"url('../img/cp-bg.png')",
+        bg2:"url('../img/cp-bg-2.png')",
+        bg3:"url('../img/payy2.png')",
+        bg3:"url('../img/payy3.png')",
+        
+      }
+
+    },
+  },
+  plugins: [],
+  darkMode:'class'
+}
+
